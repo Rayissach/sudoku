4e22e07e72c854bcc521f47af91885db57fe5668
Raymond Robinson <rayissach@Raymonds-MBP.attlocal.net>
2020-04-04 14:24:08 -0700

    Fixing any bugs. Creating function to print sudoku board

--- sudoku.py
+++ sudoku.py
@@ -13,17 +13,17 @@ board = [
 def print_board(bo):
 
     for i in range(len(bo)):
-        if i % 3 === 0 and i != 0:
-            print ("- - - - - - - - - - - - - ")
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
 
-    for j in range(len(bo)):
-        if j % 3 === 0 and j != 0:
-            print (" | ", end ="")
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print (" | ", end="")
 
-        if j == 8:
-            print(bo[i][j])
-        else:
-            print(str(bo[i][j]) + " ")
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
 
 print_board(board)
 
