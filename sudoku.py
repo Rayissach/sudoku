1c815d32b80eb67120fb996df25cab4dbfd5bc9c
Raymond Robinson <rayissach@Raymonds-MBP.attlocal.net>
2020-04-05 21:23:43 -0700

    created a function that checks to find if the input was valid

--- sudoku.py
+++ sudoku.py
@@ -10,25 +10,55 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def sudoku_board(bo):
+def solve(brd):
 
-    for x in range(len(bo)):
+    find = search_empty
+    if not find:
+        return True 
+    else:
+        row, col = find
+
+def valid(brd, num, pos):
+
+    #Check row
+    for x in range(len(brd[0])):
+        if brd[pos[0]][x] == num and pos[1] != x:
+            return False
+    
+    #Check column
+    for x in range(len(brd)):
+        if brd[x][pos[1]] == num and pos[0] != 1:
+            return False
+
+    #Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for x in range(box_y*3, box_y*3 +3):
+        for y in range(box_x*3, box_x*3 + 3):
+            if brd[x][y] == num and (x,y) != pos:
+                return False 
+
+def sudoku_board(brd):
+
+    for x in range(len(brd)):
         if x % 3 == 0 and x != 0:
             print("- - - - - - - - - - - - - ")
 
-        for y in range(len(bo[0])):
+        for y in range(len(brd[0])):
             if y % 3 == 0 and y != 0:
                 print (" | ", end="")
 
             if y == 8:
-                print(bo[x][y])
+                print(brd[x][y])
             else:
-                print(str(bo[x][y]) + " ", end="")
+                print(str(brd[x][y]) + " ", end="")
 
-sudoku_board(board)
 
-def search_empty(bo):
-    for x in range(len(bo)):
-        for y in range(len(bo[0])):
-            if bo[x][y] == 0:
+def search_empty(brd):
+    for x in range(len(brd)):
+        for y in range(len(brd[0])):
+            if brd[x][y] == 0:
                 return(x, y) #row, col
+
+    return None 
